9d6dc924e50c5b1b41043ed7afd1e08631fb03f4
KaiKiat <92348692+Kaikiat1126@users.noreply.github.com>
2024-05-20 02:43:19 +0800

    style: rename folder lib to utils

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter as FontSans } from "next/font/google";
 import '../styles/globals.css'
 import { Toaster } from "@/components/ui/toaster"
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/utils/utils"
 
 const fontSans = FontSans({
   subsets: ["latin"],
